28780a92e1293b15bf2d7da769b18ccb4b7e192f
Eureto <matek891@gmail.com>
2023-12-21 00:23:42 +0100

    copy ewelinkAPI to local folder

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+const ewelink = require('./ewelink-api-custom');
+export default async function App() {
+  console.log("-----------------start programu-----------------");
+  
+  
+});
+  /* get all devices */
+  const devices = await connection.getDevices();
+  console.log(devices);
+  
+
+
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 App that lets you open anything that is connected to SONOFF smart switch at given location
-Switch i used: 
+Switch i used: ....
